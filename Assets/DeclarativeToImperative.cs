1900cad55a3b0d4e7cf848856cd45a128d677308
kazurego7 <kazurego7@gmail.com>
2018-10-22 22:36:32 +0900

    ObservableやSyncAsyncにusingディレクティブを設定

---
+++ Assets/DeclarativeToImperative.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using static UniRx.Observable;
+using static UnitObservable;
+using static SyncAsync;
+using System;
+using System.Linq;
+
+public class DeclarativeToImperative : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		var periodFrameCount = 60;
+		var maxCount = 6;
+		void DeclarativeDo () {
+			TimerFrame (0, periodFrameCount)
+				.TakeWhile (t => t <= maxCount)
+				.SelectMany (count => PrintLog ("test"))
+				.Subscribe ();
+		}
+		void ImperativeDo () {
+			SyncLoop (new int[maxCount],
+					Sync (
+						TimerFrame (periodFrameCount).AsUnitObservable (),
+						PrintLog ("test2").AsUnitObservable ()
+					)
+				)
+				.Subscribe ();
+		}
+		IEnumerator
+		// DeclarativeDo ();
+		ImperativeDo ();
+	}
+}
--- Assets/SyncAsync.cs
+++ Assets/SyncAsync.cs
@@ -1,19 +1,34 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UniRx;
+using static UniRx.Observable;
+using static SyncAsync;
 
 public static class SyncAsync {
 	public static IObservable<T> Sync<T> (params IObservable<T>[] args) {
-		return Observable.Concat (args);
+		return Concat (args);
 	}
 	public static IObservable<T> Sync<T> (this IObservable<T> stream, params IObservable<T>[] args) {
 		return stream.Concat (args);
 	}
 	public static IObservable<T> Async<T> (params IObservable<T>[] args) {
-		return Observable.Merge (args);
+		return Merge (args);
 	}
 	public static IObservable<T> Async<T> (this IObservable<T> stream, params IObservable<T>[] args) {
-		return stream.Concat (Observable.Merge (args));
+		return stream.Concat (Merge (args));
+	}
+	public static IObservable<TR> SyncLoop<T, TR> (IEnumerable<T> items, Func<T, IObservable<TR>> proc) {
+		return items.Select (item => proc (item)).Concat ();
+	}
+	public static IObservable<TR> SyncLoop<T, TR> (IEnumerable<T> items, IObservable<TR> proc) {
+		return items.Select (_ => proc).Concat ();
+	}
+	public static IObservable<TR> AsyncLoop<T, TR> (IEnumerable<T> items, Func<T, IObservable<TR>> proc) {
+		return items.Select (item => proc (item)).Merge ();
+	}
+	public static IObservable<TR> AsyncLoop<T, TR> (IEnumerable<T> items, IObservable<TR> proc) {
+		return items.Select (_ => proc).Merge ();
 	}
 }
